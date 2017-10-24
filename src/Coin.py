ea9352d98d1d1bf5c725796aff11f6e13c04d64b
Arun Sondhi <aksondhi@gmail.com>
2017-10-24 17:21:42 -0500

    Coin now properly identifies a nickel and it's value

--- src/Coin.py
+++ src/Coin.py
@@ -3,13 +3,17 @@ __author__ = "Arun Sondhi"
 
 class Coin:
     def __init__(self, weight, diameter):
-        return
+        self.weight = weight
+        self.diameter = diameter
 
     def isValid(self):
-        return False
+        return self.weight == 5.0 and self.diameter == 0.835
 
     def getWeight(self):
-        return 2.5
+        return self.weight
 
     def getDiameter(self):
-        return 0.75
+        return self.diameter
+
+    def getValue(self):
+        return 0.05
--- src/tests.py
+++ src/tests.py
@@ -11,3 +11,11 @@ class CoinTests(unittest.TestCase):
         self.assertEqual(aCoin.isValid(), False)
         self.assertEqual(aCoin.getWeight(), 2.5)
         self.assertEqual(aCoin.getDiameter(), 0.75)
+
+    def testWhenCoinIsPassValidWeightAndDiameterTheCoinIsValidAndHasProvidedWeightAndDiameter(self):
+        nickel = Coin(5.0, 0.835)  # A nickel
+
+        self.assertEqual(nickel.isValid(), True)
+        self.assertEqual(nickel.getWeight(), 5.0)
+        self.assertEqual(nickel.getDiameter(), 0.835)
+        self.assertEqual(nickel.getValue(), 0.05)
